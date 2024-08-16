d7d21ecda1708f39669ff884163e0cb7b1ccbf68
Ansh Gupta <guptaansh6031@gmail.com>
2024-08-16 13:30:26 +0530

    designed the cart sidebar in the Navbar component

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,17 +1,36 @@
-
+"use client"
 
 
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { FiPlusCircle, FiMinusCircle } from "react-icons/fi";
+import { IoBagCheck } from "react-icons/io5";
+
 
 
 export default function Navbar() {
+
+    const togglecart = () => {
+        if (ref.current.classList.contains("translate-x-full")) {
+            ref.current.classList.remove("translate-x-full")
+            ref.current.classList.add("translate-x-0")
+        }
+
+        else {
+            ref.current.classList.remove("translate-x-0")
+            ref.current.classList.add("translate-x-full")
+        }
+    }
+
+    const ref = useRef()
+
     return (
         <div>
             <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center shadow-md">
+                <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center shadow-md">
                     <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -24,9 +43,26 @@ export default function Navbar() {
                         <Link href="/stickers" className="mr-5 hover:text-gray-900">Stickers</Link>
                         <Link href="/mugs" className="mr-5 hover:text-gray-900">Mugs</Link>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Cart
-                    <AiOutlineShoppingCart className='ml-2'/>
+                    <button onClick={togglecart} className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Cart
+                        <AiOutlineShoppingCart className='ml-2' />
                     </button>
+
+                    <div ref={ref} className="sideCart w-80 h-full absolute top-0 right-0 bg-indigo-100 px-8 py-10 transform transition-transform translate-x-full">
+                        <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+
+                        <span onClick={togglecart} className="absolute top-3 right-3 cursor-pointer text-2xl"><IoCloseCircleOutline /></span>
+
+                        <ol className='list-decimal font-normal'>
+                            <li>
+                                <div className='item flex my-4'>
+                                    <div className='w-2/3 font-medium'>T-shirt - Wear the code</div>
+                                    <div className='w-1/3 font-medium flex items-center justify-center'><FiMinusCircle className='text-lg cursor-pointer' /><span className='mx-2 text-sm'>1</span><FiPlusCircle className='text-lg cursor-pointer' /></div>
+                                </div>
+                            </li>
+                        </ol>
+
+                        <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"><IoBagCheck className='my-1 mr-1' />Checkout</button>
+                    </div>
                 </div>
             </header>
             <hr />
