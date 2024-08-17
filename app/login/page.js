77867424ab57c9d0bca023f2a4d418ee2040089d
Ansh Gupta <guptaansh6031@gmail.com>
2024-08-18 03:31:07 +0530

    handled console errors

--- app/login/page.js
+++ app/login/page.js
@@ -8,69 +8,69 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 mt-3">
-            <div class="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900"> Sign in to your account </h2>
-                <p class="mt-2 text-center text-sm text-gray-600 max-w"> Or <Link href="/signup" class="font-medium text-blue-600 hover:text-blue-500"> create an account </Link></p>
+        <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 mt-3">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900"> Sign in to your account </h2>
+                <p className="mt-2 text-center text-sm text-gray-600 max-w"> Or <Link href="/signup" className="font-medium text-blue-600 hover:text-blue-500"> create an account </Link></p>
             </div>
 
-            <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form class="space-y-6" action="#" method="POST">
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" action="#" method="POST">
                         <div>
-                            <label for="email" class="block text-sm font-medium text-gray-700"> Email address </label>
-                            <div class="mt-1">
-                                <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter your email address" />
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Email address </label>
+                            <div className="mt-1">
+                                <input id="email" name="email" type="email" autoComplete="email" required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter your email address" />
                             </div>
                         </div>
 
                         <div>
-                            <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
-                            <div class="mt-1">
-                                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter your password" />
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Password </label>
+                            <div className="mt-1">
+                                <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter your password" />
                             </div>
                         </div>
 
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-center">
-                                <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-                                <label for="remember_me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <input id="remember_me" name="remember_me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900"> Remember me </label>
                             </div>
 
-                            <div class="text-sm">
-                                <Link href="/forgot" class="font-medium text-blue-600 hover:text-blue-500"> Forgot your password? </Link>
+                            <div className="text-sm">
+                                <Link href="/forgot" className="font-medium text-blue-600 hover:text-blue-500"> Forgot your password? </Link>
                             </div>
                         </div>
 
                         <div>
-                            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Sign in </button>
+                            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Sign in </button>
                         </div>
                     </form>
 
-                    <div class="mt-6">
-                        <div class="relative">
-                            <div class="absolute inset-0 flex items-center">
-                                <div class="w-full border-t border-gray-300"></div>
+                    <div className="mt-6">
+                        <div className="relative">
+                            <div className="absolute inset-0 flex items-center">
+                                <div className="w-full border-t border-gray-300"></div>
                             </div>
-                            <div class="relative flex justify-center text-sm">
-                                <span class="px-2 bg-gray-100 text-gray-500"> Or continue with </span>
+                            <div className="relative flex justify-center text-sm">
+                                <span className="px-2 bg-gray-100 text-gray-500"> Or continue with </span>
                             </div>
                         </div>
 
-                        <div class="mt-6 grid grid-cols-3 gap-3">
+                        <div className="mt-6 grid grid-cols-3 gap-3">
                             <div>
-                                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img class="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg" alt="" />
+                                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                                    <img className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg" alt="" />
                                 </a>
                             </div>
                             <div>
-                                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img class="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg" alt="" />
+                                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                                    <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg" alt="" />
                                 </a>
                             </div>
                             <div>
-                                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img class="h-6 w-6" src="https://www.svgrepo.com/show/506498/google.svg" alt="" />
+                                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                                    <img className="h-6 w-6" src="https://www.svgrepo.com/show/506498/google.svg" alt="" />
                                 </a>
                             </div>
                         </div>
--- app/order/page.js
+++ app/order/page.js
@@ -6,42 +6,42 @@ import React from 'react'
 
 export default function Order() {
     return (
-        <section class="text-gray-600 body-font overflow-hidden mt-7">
-            <div class="container px-5 py-24 mx-auto">
-                <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                    <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-                        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4 mt-2">Order Id: #8686</h1>
-                        <p class="leading-relaxed mb-5">Congratulations!!! Your order has been Successfully placed...</p>
+        <section className="text-gray-600 body-font overflow-hidden mt-7">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                    <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                        <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4 mt-2">Order Id: #8686</h1>
+                        <p className="leading-relaxed mb-5">Congratulations!!! Your order has been Successfully placed...</p>
 
-                        <div class="flex mb-1 font-semibold justify-evenly">
-                            <a class="flex-grow border-gray-300 py-2 text-lg px-1">Item Description</a>
-                            <a class="flex-grow border-gray-300 py-2 text-lg px-1 text-center">Quantity</a>
-                            <a class="flex-grow border-gray-300 py-2 text-lg px-1 text-end">Item Total</a>
-                        </div>
-
-                        <div class="flex border-t border-gray-200 py-2">
-                            <span class="text-gray-500">Coding T-shirt</span>
-                            <span class="ml-auto text-gray-900">1</span>
-                            <span class="ml-auto text-gray-900">₹499</span>
-                        </div>
-                        <div class="flex border-t border-gray-200 py-2">
-                            <span class="text-gray-500">Coding T-shirt</span>
-                            <span class="ml-auto text-gray-900">1</span>
-                            <span class="ml-auto text-gray-900">₹499</span>
-                        </div>
-                        <div class="flex border-t border-gray-200 py-2">
-                            <span class="text-gray-500">Coding T-shirt</span>
-                            <span class="ml-auto text-gray-900">1</span>
-                            <span class="ml-auto text-gray-900">₹499</span>
+                        <div className="flex mb-1 font-semibold justify-evenly">
+                            <a className="flex-grow border-gray-300 py-2 text-lg px-1">Item Description</a>
+                            <a className="flex-grow border-gray-300 py-2 text-lg px-1 text-center">Quantity</a>
+                            <a className="flex-grow border-gray-300 py-2 text-lg px-1 text-end">Item Total</a>
                         </div>
 
-                        <div class="flex mt-5">
-                            <span class="title-font font-medium text-2xl text-gray-900">Subtotal: ₹1,508.00</span>
-                            <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Track Order</button>
+                        <div className="flex border-t border-gray-200 py-2">
+                            <span className="text-gray-500">Coding T-shirt</span>
+                            <span className="ml-auto text-gray-900">1</span>
+                            <span className="ml-auto text-gray-900">₹499</span>
+                        </div>
+                        <div className="flex border-t border-gray-200 py-2">
+                            <span className="text-gray-500">Coding T-shirt</span>
+                            <span className="ml-auto text-gray-900">1</span>
+                            <span className="ml-auto text-gray-900">₹499</span>
+                        </div>
+                        <div className="flex border-t border-gray-200 py-2">
+                            <span className="text-gray-500">Coding T-shirt</span>
+                            <span className="ml-auto text-gray-900">1</span>
+                            <span className="ml-auto text-gray-900">₹499</span>
+                        </div>
+
+                        <div className="flex mt-5">
+                            <span className="title-font font-medium text-2xl text-gray-900">Subtotal: ₹1,508.00</span>
+                            <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Track Order</button>
                         </div>
                     </div>
-                    <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
+                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
                 </div>
             </div>
         </section>
