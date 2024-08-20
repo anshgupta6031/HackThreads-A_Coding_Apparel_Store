b2f29fe63ada1b68512fee3e04e5d94e7b9d03b3
Ansh Gupta <guptaansh6031@gmail.com>
2024-08-20 16:42:25 +0530

    Changed the getproducts route in order to fetch only those tshirts which are available. Implemented the availabe color and size feature in the tshirts page.

--- app/tshirts/Client.js
+++ app/tshirts/Client.js
@@ -12,15 +12,31 @@ export default function Client(props) {
             <section className="text-gray-600 body-font mt-32">
                 <div className="container px-5 mx-auto">
                     <div className="flex flex-wrap mx-9 my-16 gap-5">
-                        {props.products.products.map((product) => {
-                            return product.category==="T-shirt" && <Link key={product._id} href={`/product/${product.slug}`}><div className="p-4 w-full shadow-lg">
+                        {Object.keys(props.products).map((product) => {
+                            return <Link key={props.products[product]._id} href={`/product/${props.products[product].slug}`}><div className="p-4 w-full shadow-lg">
                                 <div className="block relative rounded overflow-hidden">
-                                    <img alt="ecommerce" className="h-[45vh] w-[19.7vw] m-auto block" src={product.img} />
+                                    <img alt="ecommerce" className="h-[45vh] w-[19.7vw] m-auto block" src={props.products[product].img} />
                                 </div>
                                 <div className="mt-4 text-center">
                                     <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirt</h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
-                                    <p className="mt-1">₹{product.price}</p>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">{props.products[product].title}</h2>
+                                    <p className="mt-1">₹{props.products[product].price}</p>
+
+                                    <div className="mt-1">
+                                        {props.products[product].size.includes('S') && <span className='border border-gray-300 px-1 mx-1'>S</span>}
+                                        {props.products[product].size.includes('L') && <span className='border border-gray-300 px-1 mx-1'>L</span>}
+                                        {props.products[product].size.includes('M') && <span className='border border-gray-300 px-1 mx-1'>M</span>}
+                                        {props.products[product].size.includes('XL') && <span className='border border-gray-300 px-1 mx-1'>XL</span>}
+                                        {props.products[product].size.includes('XXL') && <span className='border border-gray-300 px-1 mx-1'>XXL</span>}
+                                    </div>
+
+                                    <div className="mt-1">
+                                        {props.products[product].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {props.products[product].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {props.products[product].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {props.products[product].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                        {props.products[product].color.includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                                    </div>
                                 </div>
                             </div></Link>
                         })}
--- app/tshirts/page.js
+++ app/tshirts/page.js
@@ -5,12 +5,41 @@
 
 import React from 'react'
 import Client from './Client'
+import Product from "@/models/Product";
+import connectToMongo from "@/middleware/mongoose";
 
 
 export default async function Tshirts() {
 
-    let data = await fetch(`http://localhost:3000/api/getproducts`)
-    let products = await data.json()
+    await connectToMongo()
+
+    let data = await Product.find({ category: "T-shirt" })
+
+    let tshirts = {}
+
+    for (let item of data) {
+
+        if (item.title in tshirts) {
+            if (!tshirts[item.title].color.includes(item.color) && item.availableQty > 0) {
+                tshirts[item.title].color.push(item.color)
+            }
+
+            if (!tshirts[item.title].size.includes(item.size) && item.availableQty > 0) {
+                tshirts[item.title].size.push(item.size)
+            }
+        }
+
+        else {
+            tshirts[item.title] = JSON.parse(JSON.stringify(item))
+
+            if (item.availableQty > 0) {
+                tshirts[item.title].color = [item.color]
+                tshirts[item.title].size = [item.size]
+            }
+        }
+    }
+
+    let products = await JSON.parse(JSON.stringify(tshirts))
 
     return (
         <Client products={products} />
